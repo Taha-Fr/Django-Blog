284cb7589edfea08c69e43cd10dde22ef93b0952
Taha-Fr <fraademnati.t@gmail.com>
2024-04-28 15:01:43 +0200

    "Commit to have a simple view"

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from .views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('/')
+    path("",index),
 ]
